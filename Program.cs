2aae8e02ed6dd66d41f785a91cd24a3b7bc861bc
AndersonRochaJC <jmcreggae@gmail.com>
2021-09-04 00:51:07 -0300

    solucção de exercios Cebolinha

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exerc_cio_Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fraseDigitada, fraseComDislalia;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("Digite uma flase: ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            fraseDigitada = Console.ReadLine();
+
+          fraseComDislalia = fraseDigitada
+          .Replace("r" , "l").Replace("R" , "L").Replace("a" ,"4")
+          .Replace("A" , "4").Replace("e" , "3").Replace("E" , "3");
+          Console.BackgroundColor = ConsoleColor.Yellow;
+           Console.ForegroundColor = ConsoleColor.Black;
+          Console.WriteLine(fraseComDislalia);
+          Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.White;
+        }
+        
+    }
+}
